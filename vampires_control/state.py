9e22455f1fa1bb4a54c1b815c99f16c00364b7cc
Miles Lucas <mdlucas@hawaii.edu>
2022-04-03 08:39:30 +0000

    get simple reading and writing working

--- vampires_control/state.py
+++ vampires_control/state.py
@@ -15,9 +15,10 @@ class VAMPIRES:
     This class acts as middleware between the top-level VAMPIRES commands (e.g., `vampires_beamsplitter`) and the serial or library commands. As middleware, it performs logging, updating of a local JSON state file, and updating of the SCExAO redis database.
     """
 
-    def __init__(self, local_state=LOCAL_STATE_FILE, redis_host=None, redis_port=None):
+    def __init__(self, local_state=LOCAL_STATE_FILE, redis_host=REDIS_DB_HOST, redis_port=REDIS_DB_PORT):
         self.local_state_file = local_state
-        self.state_dict = json.load(self.local_state_file)
+        with open(self.local_state_file) as fh:
+            self.state_dict = json.load(fh)
         self.redis_host = redis_host
         self.redis_port = redis_port
         self.rdb = Redis(host=self.redis_host, port=self.redis_port)
@@ -38,3 +39,7 @@ class VAMPIRES:
 
     def __repr__(self):
         return repr(self.state_dict)
+
+    def update(self):
+        with open(self.local_state_file) as fh:
+            self.state_dict = json.load(fh)
