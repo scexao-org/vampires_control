380163fa7bbe1266fccf6cfbcc9541f33eedfcdb
mileslucas <mdlucas@hawaii.edu>
2022-04-02 21:14:57 -1000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# VAMPIRES control
+
+**WARNING:** Work in progress
+
+Maintainer: [Miles Lucas](https://github.com/mileslucas)
+
+## Installation
+
+This software relies on [scxkw](https://github.com/scexao-org/SCeXaoKeyWords), please follow the installation instructions there before installing this code.
+
+```
+$ git clone https://github.com/scexao-org/vampires-control
+$ pip install vampires-control
+```
+
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+import re
+
+version = ""
+
+with open(os.path.join("vampires_control", "__init__.py"), "r") as fh:
+    for line in fh.readlines():
+        m = re.search("__version__ = [\"'](.+)[\"']", line)
+        if m:
+            version = m.group(1)
+
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    name="vampires-control",
+    version=version,
+    description="VAMPIRES control software",
+    python_requires=">=3.7,<3.10",
+    project_urls={
+        "repository": "https://github.com/scexao-org/vampires-control",
+    },
+    author="Miles Lucas",
+    author_email="mdlucas@hawaii.edu",
+    maintainer="Miles Lucas <mdlucas@hawaii.edu",
+    license="MIT",
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    package_data={},
+    scripts=[
+        "scripts/vampires_beamsplitter",
+        "scripts/vampires_diffwheel",
+        "scripts/vampires_focus"
+    ],
+    install_requires=[
+        "tqdm==4.*",
+    ],
+)
