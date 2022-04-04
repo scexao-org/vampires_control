743c044f82a74d3b2f047f08ca9b5b50447e74f3
Miles Lucas <mdlucas@hawaii.edu>
2022-04-04 10:29:01 +0000

    add offsets for qwps

--- vampires_control/devices/devices.py
+++ vampires_control/devices/devices.py
@@ -1,4 +1,5 @@
 import json
+from serial import Serial
 
 from ..state import VAMPIRES
 from .conex import ConexDevice
@@ -15,6 +16,9 @@ __all__ = [
 with open("/etc/vampires-control/device_addresses.json") as fh:
     DEVICE_MAP = json.load(fh)
 
+with open("/etc/vampires-control/conf_qwp.json") as fh:
+    QWP_OFFSETS = json.load(fh)
+
 
 class VAMPIRESBeamsplitter:
     def __init__(
@@ -24,9 +28,9 @@ class VAMPIRESBeamsplitter:
     ):
         self.name = name
         self.beamsplitter_wheel = ConexDevice(
-            "beamsplitter_wheel",
-            DEVICE_MAP["beamsplitter_wheel"],
-            keyword="beamsplitter_wheel_angle",
+            "beamsplitter",
+            DEVICE_MAP["beamsplitter"],
+            keyword="beamsplitter_angle",
             unit="deg",
         )
         self.conf_file = conf_file
@@ -37,7 +41,7 @@ class VAMPIRESBeamsplitter:
         idx = position - 1  # idx starts at 0
         values = self.positions["positions"][idx]
         self.beamsplitter_wheel.move_absolute(values["angle"], wait=wait)
-        VAMPIRES["beamsplitter_wheel"] = values["number"]
+        VAMPIRES["beamsplitter"] = values["number"]
 
     def write(self, file=None):
         if file is None:
@@ -56,13 +60,13 @@ class VAMPIRESDifferentialFilter:
     def __init__(
         self,
         name="diffwheel",
-        conf_file="/etc/vampires-control/conf_differential_filter.json",
+        conf_file="/etc/vampires-control/conf_diffwheel.json",
     ):
         self.name = name
         self.diffwheel = ConexDevice(
-            "differential_filter_wheel",
-            DEVICE_MAP["differential_filter_wheel"],
-            keyword="differential_filter_wheel_angle",
+            "diffwheel",
+            DEVICE_MAP["diffwheel"],
+            keyword="diffwheel_angle",
             unit="deg",
         )
         self.conf_file = conf_file
@@ -73,7 +77,7 @@ class VAMPIRESDifferentialFilter:
         idx = position - 1  # idx starts at 0
         values = self.positions["positions"][idx]
         self.diffwheel.move_absolute(values["angle"], wait=wait)
-        VAMPIRES["differential_filter"] = values["number"]
+        VAMPIRES["diffwheel"] = values["number"]
 
     def write(self, file=None):
         if file is None:
@@ -124,11 +128,47 @@ class VAMPIRESPupilWheel:
             self.positions = json.load(fh)
 
 
+class VAMPIRESQWP(ConexDevice):
+    def __init__(self, *args, offset=0, **kwargs):
+        self.offset = offset
+        return super().__init__(*args, **kwargs)
+
+    def target_position(self):
+        val = super().target_position()
+        return val + self.offset
+
+    def true_position(self):
+        cmd = f"1TP?\r\n".encode()
+        self.logger.debug(f"TRUE POSITION command: {cmd}")
+        with Serial(**self.serial_config) as serial:
+            serial.write(cmd)
+            retval = serial.read(1024).decode("utf-8")
+            self.logger.debug(f"returned value: {retval}")
+        # cut off leading command
+        value = float(retval[3:])
+        value += self.offset
+        if self.keyword is not None:
+            VAMPIRES[self.keyword] = value
+        return value
+
+
 beamsplitter = VAMPIRESBeamsplitter()
 differential_filter = VAMPIRESDifferentialFilter()
 pupil_wheel = VAMPIRESPupilWheel()
-qwp_1 = ConexDevice("qwp_1", DEVICE_MAP["qwp_1"], keyword="qwp_1_angle", unit="deg")
-qwp_2 = ConexDevice("qwp_2", DEVICE_MAP["qwp_2"], keyword="qwp_2_angle", unit="deg")
+qwp_1 = VAMPIRESQWP(
+    "qwp_1",
+    DEVICE_MAP["qwp_1"],
+    offset=QWP_OFFSETS["qwp_1_offset"],
+    keyword="qwp_1",
+    unit="deg",
+)
+qwp_2 = VAMPIRESQWP(
+    "qwp_2",
+    DEVICE_MAP["qwp_2"],
+    offset=QWP_OFFSETS["qwp_2_offset"],
+    keyword="qwp_2",
+    unit="deg",
+)
 focus = ConexDevice(
     "focus_stage", DEVICE_MAP["focus_stage"], keyword="focus_stage", unit="mm"
 )
