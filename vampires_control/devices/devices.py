4cb41b37307df866940da49cf269491bd553a87e
Miles Lucas <mdlucas@hawaii.edu>
2022-04-04 09:16:38 +0000

    add pupil_wheel class

--- vampires_control/devices/devices.py
+++ vampires_control/devices/devices.py
@@ -3,7 +3,14 @@ import json
 from ..state import VAMPIRES
 from .conex import ConexDevice
 
-__all__ = ["beamsplitter", "focus", "differential_filter", "qwp_1", "qwp_2"]
+__all__ = [
+    "beamsplitter",
+    "focus",
+    "differential_filter",
+    "qwp_1",
+    "qwp_2",
+    "pupil_wheel",
+]
 
 with open("/etc/vampires-control/device_addresses.json") as fh:
     DEVICE_MAP = json.load(fh)
@@ -81,8 +88,45 @@ class VAMPIRESDifferentialFilter:
             self.positions = json.load(fh)
 
 
+class VAMPIRESPupilWheel:
+    def __init__(
+        self,
+        name="pupil_wheel",
+        conf_file="/etc/vampires-control/conf_pupil_wheel.json",
+    ):
+        self.name = name
+        self.pupil_wheel = ConexDevice(
+            "pupil_wheel",
+            DEVICE_MAP["pupil_wheel"],
+            keyword="pupil_wheel_angle",
+            unit="deg",
+        )
+        self.conf_file = conf_file
+        with open(self.conf_file) as fh:
+            self.positions = json.load(fh)
+
+    def move_position(self, position: int, wait=False):
+        idx = position - 1  # idx starts at 0
+        values = self.positions["positions"][idx]
+        self.pupil_wheel.move_absolute(values["angle"], wait=wait)
+        VAMPIRES["pupil_wheel"] = values["number"]
+
+    def write(self, file=None):
+        if file is None:
+            file = self.conf_file
+        with open(file, "w") as fh:
+            json.dump(self.positions)
+
+    def update(self, file=None):
+        if file is None:
+            file = self.conf_file
+        with open(file) as fh:
+            self.positions = json.load(fh)
+
+
 beamsplitter = VAMPIRESBeamsplitter()
 differential_filter = VAMPIRESDifferentialFilter()
+pupil_wheel = VAMPIRESPupilWheel()
 qwp_1 = ConexDevice("qwp_1", DEVICE_MAP["qwp_1"], keyword="qwp_1_angle", unit="deg")
 qwp_2 = ConexDevice("qwp_2", DEVICE_MAP["qwp_2"], keyword="qwp_2_angle", unit="deg")
 focus = ConexDevice(
