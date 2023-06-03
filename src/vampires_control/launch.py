9ebf55b644fe14b21e94642a87b06b61b1312b3c
Miles Lucas <mdlucas@hawaii.edu>
2023-06-03 23:26:24 +0000

    switch to using mprocs to launch daemons

--- src/vampires_control/launch.py
+++ src/vampires_control/launch.py
@@ -1,39 +1,21 @@
 from swmain.infra.tmux import find_or_create, kill_running, send_keys
+from vampires_control import conf_dir
+import click
+from pathlib import Path
 
-
-def main():
+@click.option("-c", "--config", default=conf_dir / "daemons" / "vampires_mprocs.yml", type=click.Path(exists=True, readable=True, path_type=Path))
+@click.option("-p", "--port", default="127.0.0.1:8792")
+@click.command("launch_daemons")
+def main(port, config):
     # Device control daemon
-    print("Initializing the device control daemon")
-    devctrl_name = "vampires_control"
-    devctrl_pane = find_or_create(devctrl_name)
-    kill_running(devctrl_pane)
-    send_keys(devctrl_pane, "ipython -i -m device_control.daemons.vampires_daemon")
-    print("Device control daemon launching.")
-    print(f"Access device interfaces directly from tmux session `{devctrl_name}`")
-
-    # temperature poller
-    print("Initializing the temperature polling daemon")
-    tc_status_name = "vampires_temp_daemon"
-    tc_status_pane = find_or_create(tc_status_name)
-    kill_running(tc_status_pane)
-    send_keys(tc_status_pane, "python -m vampires_control.daemons.temp_poll_daemon")
-    print(f"Temperature polling daemon launching in pane `{tc_status_name}`.")
-
-    # QWP daemon
-    print("Initializing the QWP tracking daemon in FILTER mode")
-    qwp_name = "vampires_qwp_daemon"
-    qwp_pane = find_or_create(qwp_name)
-    kill_running(qwp_pane)
-    send_keys(qwp_pane, "python -m vampires_control.daemons.qwp_daemon")
-    print(f"QWP tracking daemon launching in pane `{qwp_name}`.")
-
-    # Gen2 daemon
-    print("Initializing the Gen2 daemon")
-    gen2_name = "vampires_gen2_daemon"
-    gen2_pane = find_or_create(gen2_name)
-    kill_running(gen2_pane)
-    send_keys(gen2_pane, "python -m vampires_control.gen2.main")
-    print(f"Gen2 daemon launching in pane `{gen2_name}`.")
+    print(f"Initializing mprocs server from {config}")
+    name = "daemons"
+    session = find_or_create(name)
+    kill_running(session)
+    command = f"mrpocs --server {port} --config {config.absolute()}"
+    # print(command)
+    send_keys(session, command)
+    print(f"mprocs server launching in tmux session `{name}`.")
 
 
 if __name__ == "__main__":
