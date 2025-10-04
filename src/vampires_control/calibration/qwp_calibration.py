14e8080d36974514ed1d519f510662e9387c7540
Miles Lucas <mdlucas@hawaii.edu>
2025-10-04 01:24:36 +0000

    update qwp grid search app

--- src/vampires_control/calibration/qwp_calibration.py
+++ src/vampires_control/calibration/qwp_calibration.py
@@ -1,10 +1,15 @@
 import logging
+import time
+from concurrent.futures import ThreadPoolExecutor
 
 import click
 import numpy as np
 import tqdm.auto as tqdm
+from device_control.facility import ImageRotator
 from pyMilk.interfacing.isio_shmlib import SHM
 from swmain.network.pyroclient import connect
+
+from vampires_control.acquisition.manager import VCAMLogManager
 
 # set up logging
 formatter = logging.Formatter("%(asctime)s|%(name)s|%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
@@ -16,70 +21,76 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
 
-class QWPOptimizer:
+class QWPSweeper:
     """
-    QWPOptimizer
+    QWPSweeper
     """
+
+    ANGLES = np.arange(0, 180, 5)
+    IMR_ANGLES = np.linspace(60, 120, 7)
 
     def __init__(self):
         self.cameras = {1: connect("VCAM1"), 2: connect("VCAM2")}
         self.shms = {1: SHM("vcam1"), 2: SHM("vcam2")}
-        self.beamsplitter = connect("VAMPIRES_BS")
-        self.filter = connect("VAMPIRES_FILT")
-        self.qwp1 = connect("VAMPIRES_QWP1")
-        self.qwp2 = connect("VAMPIRES_QWP2")
+        self.managers = {i: VCAMLogManager(i) for i in (1, 2)}
+        self.vis_qwp = connect("VIS_QWP")
+        self.imr = ImageRotator.connect()
 
-    def run(self, num_frames=5):
+    def run(self):
         logger.info("Beginning QWP calibration")
-        # check if beamsplitter is inserted
-        _, bs_config = self.beamsplitter.get_configuration()
-        logger.debug(f"beamsplitter: {bs_config}")
-        if bs_config.upper() != "PBS":
-            # if beamsplitter is not inserted, prompt
-            logger.warn("Polarizing beamsplitter is not inserted")
-            click.confirm("Would you like to insert the beamsplitter?", abort=True, default=True)
-            # insert beamsplitter
-            logger.info("Inserting PBS beamsplitter")
-            self.beamsplitter.move_configuration_name("PBS")
 
-        result_dict = {}
-        qwp_angles = None
-        for config in tqdm.tqdm(self.filter.get_configurations(), desc="Filter"):
-            if config["name"] == "625-50":
-                continue
-            self.filter.move_configuration_idx(config["idx"])
-            logger.info(f"Moving filter to {config['name']}")
-            # prepare cameras
-            click.confirm("Adjust camera settings and confirm when ready", default=True, abort=True)
-            qwp_angles, metrics = self.get_values_for_filter(num_frames=num_frames)
-            result_dict[config["name"]] = metrics
+        # prepare cameras
+        click.confirm("Adjust camera settings and confirm when ready", default=True, abort=True)
 
-        return qwp_angles, result_dict
+        imr_pbar = tqdm.tqdm(self.IMR_ANGLES, desc="IMR")
+        parity_flip1 = False
+        parity_flip2 = False
+        for imr_ang in imr_pbar:
+            self.move_imr(imr_ang)
+            qwp1_angs = self.ANGLES[::-1] if parity_flip1 else self.ANGLES
+            qwp1_pbar = tqdm.tqdm(qwp1_angs, desc="QWP1", leave=False)
+            for qwp1 in qwp1_pbar:
+                self.vis_qwp.move_absolute("1", qwp1)
+                self.shms[1].update_keyword("U_QWP1", qwp1)
+                self.shms[2].update_keyword("U_QWP1", qwp1)
 
-    def get_values_for_filter(self, num_frames=5):
-        qwp_angles = np.arange(0, 181, 5)
-        metrics = np.empty((len(qwp_angles), len(qwp_angles)))
-        for i, ang1 in enumerate(tqdm.tqdm(qwp_angles, desc="Scanning QWP1", leave=False)):
-            logger.info(f"Moving QWP1 to {ang1:3.01f} deg")
-            self.qwp1.move_absolute(ang1)
-            # reverse every other qwp2 angle
-            qwp2_angles = qwp_angles
-            if i % 2:
-                qwp2_angles = reversed(qwp_angles)
-            for j, ang2 in enumerate(tqdm.tqdm(qwp2_angles, desc="Scanning QWP2", leave=False)):
-                logger.info(f"Moving QWP2 to {ang2:3.01f} deg")
-                self.qwp2.move_absolute(ang2)
-                cube = self.shms[1].multi_recv_data(num_frames, output_as_cube=True)
-                metrics[i, j] = np.median(cube, overwrite_input=True)
-            if i % 2:
-                metrics[i] = metrics[i][::-1]
+                qwp2_angs = self.ANGLES[::-1] if parity_flip2 else self.ANGLES
+                qwp2_pbar = tqdm.tqdm(qwp2_angs, desc="QWP2", leave=False)
+                for qwp2 in qwp2_pbar:
+                    imr_pbar.write(f"IMR: {imr_ang} QWP1: {qwp1} QWP2: {qwp2}")
+                    self.vis_qwp.move_absolute("2", qwp2)
+                    self.shms[1].update_keyword("U_QWP2", qwp2)
+                    self.shms[2].update_keyword("U_QWP2", qwp2)
+                    imr_pbar.write("Taking a cube")
+                    self.take_one_cube()
 
-        return qwp_angles, metrics
+                    parity_flip2 = not parity_flip2
+            parity_flip1 = not parity_flip1
+
+    def move_imr(self, angle):
+        # move image rotator to position
+        self.imr.move_absolute(angle)
+        while np.abs(self.imr.get_position() - angle) > 0.01:
+            time.sleep(0.5)
+        # let it settle so FITS keywords are sensible
+        time.sleep(0.5)
+        self.imr.get_position()
+
+    def take_one_cube(self):
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            jobs = []
+            for mgr in self.managers.values():
+                job = executor.submit(mgr.acquire_cubes, 1)
+                jobs.append(job)
+            for job in jobs:
+                job.result()
 
 
-def get_focus_from_metric(focus, metrics):
-    # fit quadratic to curve
-    poly = np.polynomial.Polynomial.fit(focus, metrics, deg=2)
-    # vertex is at -b / 2a
-    coefs = poly.convert().coef
-    return -coefs[1] / (2 * coefs[2])
+@click.command("qwp_sweep")
+def main():
+    sweeper = QWPSweeper()
+    sweeper.run()
+
+
+if __name__ == "__main__":
+    main()
